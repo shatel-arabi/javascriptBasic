0f057a96051b17ec1e37801b9ac392e1d4a8da39
Shatel Arabi <arabi.shat@gmail.com>
2024-11-18 09:49:56 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var number = 30;
+console.log(number);
+
+var today = new Date();
+// string variable
+var name = "Arabi";
+console.log(name);
+
+// sum
+var x = 80;
+var y = 90;
+var z = x+y;
+console.log(z);
+// float number
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+console.log(total);
+
+
